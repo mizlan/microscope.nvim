c4532595674344e3441e4a43ee2e99aca06e30ff
Michael Lan <michaellan202@gmail.com>
2021-03-23 11:36:23 -0700

    Add highlight on selection and commands to move the seelection

--- experiment.lua
+++ experiment.lua
@@ -10,7 +10,47 @@ vim.fn.sign_define('microscope_prompt', {
   texthl = "Question",
   linehl = 'TabLine'
 })
-vim.fn.sign_place(1, '', 'microscope_prompt', Microscope_G.prompt_bufnr, { lnum = 1 })
+Microscope_G.PROMPT_SIGN_ID = 32
+vim.fn.sign_place(Microscope_G.PROMPT_SIGN_ID, 'microscope', 'microscope_prompt', Microscope_G.prompt_bufnr, { lnum = 1 })
+
+vim.fn.sign_define('microscope_candidate_selection', {
+  text   = "> ",
+  texthl = "StatusLine",
+  linehl = 'CursorLine'
+})
+Microscope_G.CANDIDATE_SELECTION_SIGN_ID = 69
+Microscope_G.candidate_selection_lnum = 0
+Microscope_G.is_valid_selection = function(lnum)
+  return lnum > 0 and lnum <= vim.api.nvim_buf_line_count(Microscope_G.candidates_bufnr)
+end
+Microscope_G.update_candidate_selection = function(lnum)
+  if not Microscope_G.is_valid_selection(lnum) then return nil end
+  vim.fn.sign_unplace(
+    'microscope',
+    {
+      buffer = Microscope_G.candidates_bufnr,
+      id = Microscope_G.CANDIDATE_SELECTION_SIGN_ID,
+    }
+  )
+  vim.fn.sign_place(
+    Microscope_G.CANDIDATE_SELECTION_SIGN_ID,
+    'microscope',
+    'microscope_candidate_selection',
+    Microscope_G.candidates_bufnr,
+    { lnum = lnum }
+  )
+  vim.fn.win_gotoid(Microscope_G.candidates_winnr)
+  vim.fn.execute('normal! '..lnum..'Gzz')
+  vim.fn.win_gotoid(Microscope_G.prompt_winnr)
+end
+Microscope_G.increment_selection = function(lnum)
+  Microscope_G.candidate_selection_lnum = Microscope_G.candidate_selection_lnum + 1
+  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum)
+end
+Microscope_G.decrement_selection = function(lnum)
+  Microscope_G.candidate_selection_lnum = Microscope_G.candidate_selection_lnum - 1
+  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum)
+end
 
 Microscope_G.promptline = vim.fn.floor(vim.api.nvim_get_option('lines') * 2 / 3)
 
@@ -49,7 +89,7 @@ end
 
 cmds = get_keys(vim.api.nvim_get_commands({}))
 
-results = first_(100000)(lines_from('words'))
+results = first_(200000)(lines_from('words'))
 
 --- fuzzy search (an alternative to prefix/substring search)
 Microscope_G.fuzzy = function(query, s)
@@ -103,7 +143,9 @@ Microscope_G.show_windows = function()
   })
   vim.api.nvim_win_set_option(Microscope_G.prompt_winnr, 'signcolumn', 'yes')
   vim.api.nvim_win_set_option(Microscope_G.candidates_winnr, 'signcolumn', 'yes')
-  vim.cmd('startinsert!')
+  vim.api.nvim_buf_call(Microscope_G.prompt_bufnr, function()
+    vim.cmd('startinsert!')
+  end)
   return Microscope_G.candidates_winnr, Microscope_G.prompt_winnr
 end
 
@@ -128,12 +170,15 @@ Microscope_G.attach_updater = function()
         local text = vim.api.nvim_buf_get_lines(Microscope_G.prompt_bufnr, 0, -1, true)[1]
         local allowed = Microscope_G.get_valid(text, results)
         vim.api.nvim_buf_set_lines(Microscope_G.candidates_bufnr, 0, -1, true, allowed)
+        -- TODO: fix the selected choice (if it is still present)
       end)
     end
   })
 end
 
 vim.api.nvim_set_keymap('n', '<Leader>.', ':lua Microscope_G.show_windows()<CR>', {silent = true})
-vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'n', '<Esc>', ':lua Microscope_G.close_windows()<CR>', {silent = true})
+vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<Esc>', '<Esc>:lua Microscope_G.close_windows()<CR>', {silent = true})
+vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-n>', '<Esc>:lua Microscope_G.increment_selection()<CR>a', {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-p>', '<Esc>:lua Microscope_G.decrement_selection()<CR>a', {noremap = true, silent = true})
 
 Microscope_G.attach_updater()
