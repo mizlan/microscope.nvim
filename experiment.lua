fa251f4adf8f38df809ddbd7cb97efa85708efd0
Michael Lan <michaellan202@gmail.com>
2021-03-23 16:06:28 -0700

    Fix a ton of bugs

--- experiment.lua
+++ experiment.lua
@@ -1,5 +1,8 @@
 Microscope_G = Microscope_G or {}
 
+-- DEBUG
+dbg = require('luadev').print
+
 Microscope_G.prompt_bufnr = vim.api.nvim_create_buf(true, true)
 Microscope_G.candidates_bufnr = vim.api.nvim_create_buf(true, true)
 vim.api.nvim_buf_set_option(Microscope_G.prompt_bufnr, 'buflisted', false)
@@ -10,21 +13,25 @@ vim.fn.sign_define('microscope_prompt', {
   texthl = "Question",
   linehl = 'TabLine'
 })
+
 Microscope_G.PROMPT_SIGN_ID = 32
 vim.fn.sign_place(Microscope_G.PROMPT_SIGN_ID, 'microscope', 'microscope_prompt', Microscope_G.prompt_bufnr, { lnum = 1 })
 
 vim.fn.sign_define('microscope_candidate_selection', {
   text   = "> ",
-  texthl = "StatusLine",
+  texthl = "CursorLine",
   linehl = 'CursorLine'
 })
+
 Microscope_G.CANDIDATE_SELECTION_SIGN_ID = 69
+-- if = 0, don't show
 Microscope_G.candidate_selection_lnum = 0
+
 Microscope_G.is_valid_selection = function(lnum)
   return lnum > 0 and lnum <= vim.api.nvim_buf_line_count(Microscope_G.candidates_bufnr)
 end
-Microscope_G.update_candidate_selection = function(lnum)
-  if not Microscope_G.is_valid_selection(lnum) then return nil end
+
+Microscope_G.hide_candidate_selection = function()
   vim.fn.sign_unplace(
     'microscope',
     {
@@ -32,6 +39,14 @@ Microscope_G.update_candidate_selection = function(lnum)
       id = Microscope_G.CANDIDATE_SELECTION_SIGN_ID,
     }
   )
+end
+
+Microscope_G.update_candidate_selection = function(lnum)
+  if not Microscope_G.is_valid_selection(lnum) then return nil end
+
+  Microscope_G.candidate_selection_lnum = lnum
+  Microscope_G.hide_candidate_selection()
+
   vim.fn.sign_place(
     Microscope_G.CANDIDATE_SELECTION_SIGN_ID,
     'microscope',
@@ -41,15 +56,15 @@ Microscope_G.update_candidate_selection = function(lnum)
   )
   vim.fn.win_gotoid(Microscope_G.candidates_winnr)
   vim.fn.execute('normal! '..lnum..'Gzz')
-  vim.fn.win_gotoid(Microscope_G.prompt_winnr)
+  vim.fn.execute('wincmd p')
 end
-Microscope_G.increment_selection = function(lnum)
-  Microscope_G.candidate_selection_lnum = Microscope_G.candidate_selection_lnum + 1
-  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum)
+
+Microscope_G.attempt_increment_selection = function(lnum)
+  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum + 1)
 end
-Microscope_G.decrement_selection = function(lnum)
-  Microscope_G.candidate_selection_lnum = Microscope_G.candidate_selection_lnum - 1
-  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum)
+
+Microscope_G.attempt_decrement_selection = function(lnum)
+  Microscope_G.update_candidate_selection(Microscope_G.candidate_selection_lnum - 1)
 end
 
 Microscope_G.promptline = vim.fn.floor(vim.api.nvim_get_option('lines') * 2 / 3)
@@ -89,7 +104,7 @@ end
 
 cmds = get_keys(vim.api.nvim_get_commands({}))
 
-results = first_(200000)(lines_from('words'))
+results = lines_from('words')
 
 --- fuzzy search (an alternative to prefix/substring search)
 Microscope_G.fuzzy = function(query, s)
@@ -107,21 +122,28 @@ end
 
 --- get_valid returns an array of valid matches, and caches based on the query string
 Microscope_G.get_valid_cache = {}
-Microscope_G.get_valid = function(query, candidates)
-  local matches = {}
+Microscope_G.get_valid = function(query, candidates, track)
+  -- TODO: add option to track a particular value (more efficient)
+  local track = track or '' -- or query, possibly?
+  local track_idx = nil
   -- if the value is already cached, return it
   local cache_value = Microscope_G.get_valid_cache[query]
-  if cache_value ~= nil then
-    return cache_value
+  if cache_value == nil then
+    local matches = {}
+    for i, candidate in ipairs(candidates) do
+      -- if vim.fn.strlen(query) == 0 or Microscope_G.fuzzy(vim.fn.tolower(query), vim.fn.tolower(candidate)) then
+      if vim.startswith(vim.fn.tolower(candidate), vim.fn.tolower(query)) then
+        matches[#matches+1] = candidate
+      end
+    end
+    Microscope_G.get_valid_cache[query] = matches
   end
-  for _, candidate in ipairs(candidates) do
-    if vim.fn.strlen(query) == 0 or Microscope_G.fuzzy(vim.fn.tolower(query), vim.fn.tolower(candidate)) then
-    -- if vim.startswith(candidate, query) then
-      matches[#matches+1] = candidate
+  for i, match in ipairs(Microscope_G.get_valid_cache[query]) do
+    if match == track then
+      track_idx = i
     end
   end
-  Microscope_G.get_valid_cache[query] = matches
-  return matches
+  return Microscope_G.get_valid_cache[query], track_idx
 end
 
 Microscope_G.show_windows = function()
@@ -129,7 +151,7 @@ Microscope_G.show_windows = function()
     relative = 'editor',
     row      = Microscope_G.promptline + 1,
     col      = 2,
-    width    = vim.api.nvim_get_option('columns') - 4,
+    width    = vim.api.nvim_get_option('columns') - 120,
     height   = vim.api.nvim_get_option('lines') - Microscope_G.promptline - 2,
     style    = 'minimal'
   })
@@ -137,7 +159,7 @@ Microscope_G.show_windows = function()
     relative = 'editor',
     row      = Microscope_G.promptline,
     col      = 2,
-    width    = vim.api.nvim_get_option('columns') - 4,
+    width    = vim.api.nvim_get_option('columns') - 120,
     height   = 1,
     style    = 'minimal'
   })
@@ -168,9 +190,27 @@ Microscope_G.attach_updater = function()
       end
       vim.schedule(function()
         local text = vim.api.nvim_buf_get_lines(Microscope_G.prompt_bufnr, 0, -1, true)[1]
-        local allowed = Microscope_G.get_valid(text, results)
+        local track = Microscope_G.is_valid_selection(Microscope_G.candidate_selection_lnum) and
+          -- should be equivalent to checking if Microscope_G.candidate_selection_lnum == 0
+          vim.api.nvim_buf_get_lines(
+            Microscope_G.candidates_bufnr,
+            Microscope_G.candidate_selection_lnum - 1,
+            Microscope_G.candidate_selection_lnum,
+            false
+          )[1] or nil
+        local allowed, track_idx = Microscope_G.get_valid(text, results, track)
         vim.api.nvim_buf_set_lines(Microscope_G.candidates_bufnr, 0, -1, true, allowed)
-        -- TODO: fix the selected choice (if it is still present)
+        if track_idx ~= nil then
+          Microscope_G.update_candidate_selection(track_idx)
+        elseif not Microscope_G.is_valid_selection(Microscope_G.candidate_selection_lnum) then
+          -- attempt to first set it to the top value (like ivy) or then hide it
+          if Microscope_G.is_valid_selection(1) then
+            Microscope_G.update_candidate_selection(1)
+          else
+            Microscope_G.update_candidate_selection(0)
+            Microscope_G.hide_candidate_selection()
+          end
+        end
       end)
     end
   })
@@ -178,7 +218,16 @@ end
 
 vim.api.nvim_set_keymap('n', '<Leader>.', ':lua Microscope_G.show_windows()<CR>', {silent = true})
 vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<Esc>', '<Esc>:lua Microscope_G.close_windows()<CR>', {silent = true})
-vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-n>', '<Esc>:lua Microscope_G.increment_selection()<CR>a', {noremap = true, silent = true})
-vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-p>', '<Esc>:lua Microscope_G.decrement_selection()<CR>a', {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-n>', '<Esc>:lua Microscope_G.attempt_increment_selection()<CR>a', {noremap = true, silent = true})
+vim.api.nvim_buf_set_keymap(Microscope_G.prompt_bufnr, 'i', '<C-p>', '<Esc>:lua Microscope_G.attempt_decrement_selection()<CR>a', {noremap = true, silent = true})
 
 Microscope_G.attach_updater()
+print(vim.inspect(Microscope_G.get_valid_cache["breadb"]))
+print(Microscope_G.candidate_selection_lnum)
+print(Microscope_G.is_valid_selection(Microscope_G.candidate_selection_lnum))
+print(vim.api.nvim_buf_get_lines(
+  Microscope_G.candidates_bufnr,
+  Microscope_G.candidate_selection_lnum - 1,
+  Microscope_G.candidate_selection_lnum,
+  false
+)[1])
